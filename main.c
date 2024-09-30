f65fbfdd6e1ded22dff7c13c66b45f68afed7ca8
EBP_erika <93957694+ErikaBP@users.noreply.github.com>
2024-09-30 20:50:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
-using namespace std;
-
-#define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
-
-typedef long long lli;
-
-const int INF = 0x3f3f3f3f;
-const lli LINF = 0x3f3f3f3f3f3f3f3fll;
-
-int main(){
-    printf("Hello");
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
